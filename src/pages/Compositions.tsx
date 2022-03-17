7d9e971a88a4c47a6467a46e17a4a59baf571076
Nicolas <Nicolas>
2022-03-17 16:17:41 +0100

    crunch

--- src/pages/Compositions.tsx
+++ src/pages/Compositions.tsx
@@ -1,6 +1,7 @@
 import { IonButton, IonContent, IonHeader, IonImg, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Compositions.css';
+import compofrance from './compofrance.jpeg';
 
 const Compositions: React.FC = () => {
   return (
@@ -13,8 +14,7 @@ const Compositions: React.FC = () => {
         <IonButton color="light texte"href=''>Retour</IonButton>
         <IonButton color="light texte"href='Live'>Match</IonButton>
       </div>
-        <IonImg className='background-image2 container1'>
-        </IonImg>
+      <IonImg className="" src={compofrance} />
       <div className= 'roulade'>
         <span color="light text bouton" > Avec une équipe comme ça le Rosbeef ça se mange cru ! 🤌👊 </span>
       </div>
