d2f54f15e4b467de33eddab79498f201d51a6248
Nicolas Baudouy <Nicolas@MacBook-Air-3.local>
2022-03-18 11:39:38 +0100

    Updates

---
+++ static/js/3107.873e8221.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3107.873e8221.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkthe_crunch=self.webpackChunkthe_crunch||[]).push([[3107],{3107:function(c,n,u){u.r(n),u.d(n,{g:function(){return e}});var e=u(7923).i}}]);
+//# sourceMappingURL=3107.873e8221.chunk.js.map
---
+++ static/js/3127.896aa004.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 3127.896aa004.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkthe_crunch=self.webpackChunkthe_crunch||[]).push([[3127],{3127:function(e,n,r){r.r(n),r.d(n,{i:function(){return t}});var t=function(e){return e&&""!==e.dir?"rtl"===e.dir.toLowerCase():"rtl"===(null===document||void 0===document?void 0:document.dir.toLowerCase())}}}]);
+//# sourceMappingURL=3127.896aa004.chunk.js.map
